987e06972ba0cec9fc36717c013ac06036c602e7
Dmitri Shuralyov <shurcooL@gmail.com>
2015-12-03 22:55:49 -0800

    WIP: Initial version of subscriptions and notification center updates.

--- notifications.go
+++ notifications.go
@@ -11,9 +11,12 @@ import (
 type Service interface {
 	List(ctx context.Context, opt interface{}) ([]Notification, error)
 
-	Subscribe(ctx context.Context, repo issues.RepoSpec, appID string, threadID uint64, actors []issues.UserSpec) error
+	Subscribe(ctx context.Context, appID string, repo issues.RepoSpec, threadID uint64, subscribers []issues.UserSpec) error
 
-	MarkRead(ctx context.Context, repo issues.RepoSpec, appID string, threadID uint64) error
+	MarkRead(ctx context.Context, appID string, repo issues.RepoSpec, threadID uint64) error
+
+	// TODO.
+	Create(ctx context.Context, appID string, repo issues.RepoSpec, threadID uint64, notification Notification) error
 
 	// TODO: This doesn't belong here, does it?
 	//CurrentUser(ctx context.Context) (*User, error)
@@ -25,9 +28,16 @@ type CopierFrom interface {
 
 type Notification struct {
 	RepoSpec  issues.RepoSpec
-	Type      string
+	Type      string // TODO: Give it a named type? Or something?
 	Title     string
 	HTMLURL   template.URL
 	UpdatedAt time.Time
-	State     string
+	State     string // TODO: Change to icon, etc.?
 }
+
+// Notifications implements sort.Interface.
+type Notifications []Notification
+
+func (s Notifications) Len() int           { return len(s) }
+func (s Notifications) Less(i, j int) bool { return !s[i].UpdatedAt.Before(s[j].UpdatedAt) }
+func (s Notifications) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
