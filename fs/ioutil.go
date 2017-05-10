d9b37121c5e82e2d18c367bfa25d7fb17b14c0c5
Dmitri Shuralyov <shurcooL@gmail.com>
2017-05-10 16:59:24 -0400

    fs: Use os.O_WRONLY, 0600 in jsonEncodeFile.
    vfsutil.Create uses os.O_RDWR, but we don't need read mode. It also
    uses 0644 permission, which is more broad than necessary.

--- fs/ioutil.go
+++ fs/ioutil.go
@@ -1,6 +1,7 @@
 package fs
 
 import (
+	"context"
 	"encoding/json"
 	"os"
 	pathpkg "path"
@@ -11,7 +12,7 @@ import (
 
 // jsonEncodeFile encodes v into file at path, overwriting or creating it.
 func jsonEncodeFile(fs webdav.FileSystem, path string, v interface{}) error {
-	f, err := vfsutil.Create(fs, path)
+	f, err := fs.OpenFile(context.Background(), path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
 		return err
 	}
